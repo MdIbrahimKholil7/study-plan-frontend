8313b15b726babe5fb4b57a333d7ea3931361189
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2024-03-12 00:25:17 +0600

    login signup btn design complete

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,7 +10,7 @@ interface FormData {
   email: string;
 }
 
-const SignUpPage: React.FC = () => {
+const SignUpPage = () => {
   const {
     register,
     handleSubmit,
@@ -26,7 +26,7 @@ const SignUpPage: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
-        <h2 className="text-2xl mb-4">Signup</h2>
+        <h2 className="text-2xl mb-4">{!show ? "Signup" : "Login"}</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label htmlFor="username" className="block text-gray-700">
@@ -36,26 +36,33 @@ const SignUpPage: React.FC = () => {
               type="text"
               id="username"
               {...register("username", { required: "Username is required" })}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 outline-none p-2"
+              className="mt-1 block w-full rounded-md  shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 outline-none p-2 border-blue-500 border-[0.5px]"
+              placeholder="Enter your name"
             />
             {errors.username && (
               <span className="text-red-500">{errors.username.message}</span>
             )}
           </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              {...register("email", { required: "Password is required" })}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 outline-none p-2"
-            />
-            {errors.password && (
-              <span className="text-red-500">{errors.password.message}</span>
-            )}
-          </div>
+          {!show && (
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: /^\S+@\S+$/i,
+                })}
+                className="mt-1 block w-full rounded-md border-blue-500 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 outline-none p-2 border-[0.5px]"
+                placeholder="Enter your email"
+              />
+              {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
+              )}
+            </div>
+          )}
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700">
               Password
@@ -64,20 +71,46 @@ const SignUpPage: React.FC = () => {
               type="password"
               id="password"
               {...register("password", { required: "Password is required" })}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 outline-none p-2"
+              className="mt-1 block w-full rounded-md border-blue-500 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 outline-none p-2 border-[0.5px]"
+              placeholder="Enter your password"
             />
             {errors.password && (
               <span className="text-red-500">{errors.password.message}</span>
             )}
           </div>
-          {show && (
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-            >
-              Login
-            </button>
-          )}
+          <div>
+            {!show && (
+              <div className="text-center">
+                <p>Already have an account</p>
+                <p
+                  onClick={() => setShow(!show)}
+                  className="text-blue-500 cursor-pointer hover:underline"
+                >
+                  Login
+                </p>
+              </div>
+            )}
+            {show && (
+              <div className="text-center">
+                <p className="mb-2">{`Don't`} have an account</p>
+                <p
+                  onClick={() => setShow(!show)}
+                  className="text-blue-500 cursor-pointer hover:underline"
+                >
+                  Signup
+                </p>
+              </div>
+            )}
+            {show && (
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 px-4 rounded-md mt-4 hover:bg-blue-600"
+              >
+                Login
+              </button>
+            )}
+          </div>
+
           {!show && (
             <button
               type="submit"
